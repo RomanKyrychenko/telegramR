415ba29ba56c3c6968fbdce357983626eb96752f
Roman Kyrychenko <kirichenko17roman@gmail.com>
2025-04-12 22:35:01 +0300

    Refactor AESModeCTR class to manage key, IV, and counter as separate fields; update encrypt and decrypt methods to use custom keystream generation.

--- R/aesctr.R
+++ R/aesctr.R
@@ -1,18 +1,22 @@
-#' R6 Class Representing AES CTR Mode
-#'
+#' @title R6 Class Representing AES CTR Mode
 #' @description
 #' A wrapper around AES CTR mode with custom IV using the `pyaes` library.
-#'
 #' @details
 #' This class provides methods to encrypt and decrypt data using AES CTR mode.
 #' It initializes with a key and a 16-byte initialization vector (IV).
-
+#' @export
 AESModeCTR <- R6::R6Class(
   "AESModeCTR",
   public = list(
 
-    #' @field ._aes An instance of the AESModeOfOperationCTR from the `pyaes` library.
-    ._aes = NULL,
+    #' @field ._key The encryption key.
+    ._key = NULL,
+
+    #' @field ._iv The initialization vector.
+    ._iv = NULL,
+
+    #' @field ._counter The counter for AES CTR mode.
+    ._counter = NULL,
 
     #' @description
     #' Initialize the AES CTR mode with the given key and IV.
@@ -26,8 +30,9 @@ AESModeCTR <- R6::R6Class(
       stopifnot(is.raw(key))
       stopifnot(is.raw(iv), length(iv) == 16)
 
-      self$._aes <- pyaes$AESModeOfOperationCTR(key)
-      self$._aes$._counter$._counter <- as.list(as.integer(iv))
+      self$._key <- key
+      self$._iv <- iv
+      self$._counter <- as.integer(iv)
     },
 
     #' @description
@@ -41,7 +46,19 @@ AESModeCTR <- R6::R6Class(
     #' plain_text <- as.raw(c(0x00, 0x01, 0x02, 0x03))
     #' cipher_text <- aes_ctr$encrypt(plain_text)
     encrypt = function(data) {
-      return(self$._aes$encrypt(data))
+      result <- raw(length(data))
+      for (i in seq_along(data)) {
+        # Generate keystream block
+        counter_bytes <- as.raw(self$._counter)
+        keystream <- digest::AES(self$._key, counter_bytes, mode = "ECB")
+
+        # XOR data with keystream
+        result[i] <- as.raw(bitwXor(as.integer(data[i]), as.integer(keystream[1])))
+
+        # Increment counter
+        self$._increment_counter()
+      }
+      return(result)
     },
 
     #' @description
@@ -55,7 +72,19 @@ AESModeCTR <- R6::R6Class(
     #' cipher_text <- as.raw(c(0x8d, 0x6c, 0x63, 0x7c))
     #' plain_text <- aes_ctr$decrypt(cipher_text)
     decrypt = function(data) {
-      return(self$._aes$decrypt(data))
+      return(self$encrypt(data))
+    },
+
+    #' @description
+    #' Increment the counter for the next block.
+    ._increment_counter = function() {
+      counter_vec <- self$._counter
+
+      for (i in length(counter_vec):1) {
+        counter_vec[i] <- (counter_vec[i] + 1) %% 256
+        if (counter_vec[i] != 0) break
+      }
+      self$._counter <- counter_vec
     }
   )
 )
