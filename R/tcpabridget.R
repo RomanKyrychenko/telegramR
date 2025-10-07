928ddac3d86942bc5657c0e0f9a01cd353230aff
Roman Kyrychenko <kirichenko17roman@gmail.com>
2025-10-07 19:41:38 +0300

    Enhance documentation clarity in tcpabridget.R by adding field descriptions and method parameters

--- R/tcpabridget.R
+++ R/tcpabridget.R
@@ -1,23 +1,21 @@
 #' @title AbridgedPacketCodec
 #' @description An R6 class that implements the Abridged Packet Codec for encoding and decoding packets.
 #' @inherit PacketCodec
-#' @field tag A raw byte representing the tag for the codec.
-#' @field obfuscate_tag A raw vector used for obfuscation.
-#' @method encode_packet Encodes a packet by adding a length prefix.
-#' @param data A raw vector representing the data to be encoded.
-#' @return A raw vector containing the encoded packet with the length prefix.
-#' @method read_packet Reads and decodes a packet from a reader.
-#' @param reader An object with a `readexactly` method to read a specific number of bytes.
-#' @return A promise that resolves to the decoded packet.
 #' @export
 AbridgedPacketCodec <- R6Class(
   "AbridgedPacketCodec",
   inherit = PacketCodec,
   public = list(
+
+    #' @field tag A raw byte representing the tag for the codec.
     tag = as.raw(0xef),
+
+    #' @field obfuscate_tag A raw vector used for obfuscation.
     obfuscate_tag = as.raw(c(0xef, 0xef, 0xef, 0xef)),
 
     #' @description Encodes a packet by adding a length prefix.
+    #' @param data A raw vector representing the data to be encoded.
+    #' @return A raw vector containing the encoded packet with the length prefix.
     encode_packet = function(data) {
       len_val <- bitwShiftR(length(data), 2)
       if (len_val < 127) {
@@ -32,6 +30,8 @@ AbridgedPacketCodec <- R6Class(
     },
 
     #' @description Reads and decodes a packet from a reader.
+    #' @param reader An object with a `readexactly` method to read a specific number of bytes.
+    #' @return A promise that resolves to the decoded packet.
     read_packet = function(reader) {
       promise(function(resolve, reject) {
         reader$readexactly(1) %...>% (function(first_byte) {
@@ -54,14 +54,14 @@ AbridgedPacketCodec <- R6Class(
 #' @title ConnectionTcpAbridged
 #' @description An R6 class that represents a TCP connection using the Abridged Packet Codec.
 #' @inherit Connection
-#' @method initialize Initializes the connection.
-#' @param ... Additional parameters passed to the parent class.
 #' @export
 ConnectionTcpAbridged <- R6Class(
   "ConnectionTcpAbridged",
   inherit = Connection,
   public = list(
     #' @description Initializes the connection.
+    #' @param ... Additional parameters passed to the parent class.
+    #' @return None.
     initialize = function(...) {
       super$initialize(...)
     }
