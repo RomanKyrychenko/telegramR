5aadfab43bde677338665332ba1a323f9cd2a477
Roman Kyrychenko <kirichenko17roman@gmail.com>
2025-10-07 19:37:03 +0300

    Refactor AESModeCTR class to improve documentation clarity and consistency

--- R/aesctr.R
+++ R/aesctr.R
@@ -1,23 +1,25 @@
-#' @title R6 Class Representing AES CTR Mode
+#' @title AES CTR Mode R6 Class
 #' @description
-#' A wrapper around AES CTR mode with custom IV using the `pyaes` library.
+#' A wrapper around AES CTR mode with custom IV using the `digest` library.
+#'
 #' @details
 #' This class provides methods to encrypt and decrypt data using AES CTR mode.
 #' It initializes with a key and a 16-byte initialization vector (IV).
+#'
 #' @export
 AESModeCTR <- R6::R6Class(
   "AESModeCTR",
   public = list(
 
-    #' @field ._key The encryption key.
-    ._key = NULL,
+    #' @field key The encryption key as a raw vector.
+    key = NULL,
 
-    #' @field ._iv The initialization vector.
-    ._iv = NULL,
+    #' @field iv The initialization vector (IV) as a 16-byte raw vector.
+    iv = NULL,
 
     #' @description
     #' Initialize the AES CTR mode with the given key and IV.
-    #' @param key A raw vector representing the encryption key.
+    #' @param key A raw vector representing the encryption key (16 bytes).
     #' @param iv A raw vector representing the initialization vector (16 bytes).
     #' @examples
     #' key <- as.raw(rep(0x01, 16))
@@ -28,9 +30,8 @@ AESModeCTR <- R6::R6Class(
       stopifnot(is.raw(iv))
       stopifnot(length(iv) == 16)
       stopifnot(length(key) == 16)
-
-      self$._key <- key
-      self$._iv <- iv
+      self$key <- key
+      self$iv <- iv
     },
 
     #' @description
@@ -44,8 +45,8 @@ AESModeCTR <- R6::R6Class(
     #' plain_text <- as.raw(c(0x00, 0x01, 0x02, 0x03))
     #' cipher_text <- aes_ctr$encrypt(plain_text)
     encrypt = function(data) {
-      aes <- digest::AES(self$._key, mode = "CTR", IV = self$._iv)
-      return(aes$encrypt(data))
+      aes <- digest::AES(self$key, mode = "CTR", IV = self$iv)
+      aes$encrypt(data)
     },
 
     #' @description
@@ -56,11 +57,11 @@ AESModeCTR <- R6::R6Class(
     #' key <- as.raw(rep(0x01, 16))
     #' iv <- as.raw(rep(0x02, 16))
     #' aes_ctr <- AESModeCTR$new(key, iv)
-    #' cipher_text <- as.raw(c(0x8d, 0x6c, 0x63, 0x7c))
+    #' cipher_text <- aes_ctr$encrypt(as.raw(c(0x00, 0x01, 0x02, 0x03)))
     #' plain_text <- aes_ctr$decrypt(cipher_text)
     decrypt = function(data) {
-      aes <- digest::AES(self$._key, mode = "CTR", IV = self$._iv)
-      return(aes$decrypt(data, raw = TRUE))
+      aes <- digest::AES(self$key, mode = "CTR", IV = self$iv)
+      aes$decrypt(data, raw = TRUE)
     }
   )
 )
