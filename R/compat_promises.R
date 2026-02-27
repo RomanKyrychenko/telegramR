9705a8055757f6c1ddd61a408894be88a26a8ffa
Roman Kyrychenko <kirichenko17roman@gmail.com>
2026-02-27 20:13:54 +0200

    Add value and skip_if_no_integration utilities: wrap future::value; add integration test skip helper

--- R/compat_promises.R
+++ R/compat_promises.R
@@ -2,3 +2,15 @@ promise <- promises::promise
 promise_resolve <- promises::promise_resolve
 `%...>%` <- promises::`%...>%`
 `%...!%` <- promises::`%...!%`
+value <- function(x, ...) {
+  future::value(x, ...)
+}
+
+skip_if_no_integration <- function() {
+  if (!nzchar(Sys.getenv("TELEGRAMR_INTEGRATION"))) {
+    if (requireNamespace("testthat", quietly = TRUE)) {
+      testthat::skip("Integration tests disabled; set TELEGRAMR_INTEGRATION=1")
+    }
+  }
+  invisible(NULL)
+}
